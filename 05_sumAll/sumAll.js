63bc30d24f744d888ac79edc55d6542cde40a16f
Crispon500 <crispon500@gmail.com>
2023-03-11 06:06:18 -0600

    exercise 5 complete with all unit tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    //check for the bigger number
+    if(typeof a !== "number" || typeof b !== "number" ||
+        a < 0 || b < 0){
+        return "ERROR";
+    }
 
+    let big = 0;
+    let small = 0;
+    if(a > b){
+        big = a;
+        small = b;
+    } 
+    else if(a < b){
+        big = b;
+        small = a;
+    }
+    else{
+        return a;
+    }
+    result = 0;
+    for(let i = small; i <= big; i++){
+        result += i;
+    }
+    console.log(result);
+    return result;
+    
 };
 
 // Do not edit below this line
