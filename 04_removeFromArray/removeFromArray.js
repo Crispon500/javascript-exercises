945b7ca228a39ddf4019d76a0596b98d516623a7
Crispon500 <crispon500@gmail.com>
2023-03-11 05:04:59 -0600

    unit test 4 exercise complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    const arr = args[0];
+    args.shift();
+    for(let i = 0; i < args.length; i++){
+        if(arr.indexOf(args[i])){
+            arr.splice(arr.indexOf(args[i]), 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
