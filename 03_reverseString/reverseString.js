7700b33defae15322f51d8a45b74b01241a41877
Crispon500 <crispon500@gmail.com>
2023-03-11 01:32:17 -0600

    unit test practice 3 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitString = str.split("");
+    let reverseArr = splitString.reverse();
+    return reverseArr.join("");
 };
 
 // Do not edit below this line
