67afc4784f00ad78b8faec0e6e248b60fe4cdb34
Crispon500 <crispon500@gmail.com>
2023-03-11 05:40:30 -0600

    complete exercise 4 with all unit tests passes

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,7 +2,7 @@ const removeFromArray = function(...args) {
     const arr = args[0];
     args.shift();
     for(let i = 0; i < args.length; i++){
-        if(arr.indexOf(args[i])){
+        if(arr.indexOf(args[i]) >= 0){
             arr.splice(arr.indexOf(args[i]), 1);
         }
     }
