437227187d2db3387ecbbf5ece671f244f881655
Crispon500 <crispon500@gmail.com>
2023-03-15 02:30:02 -0500

    exercise 8 complete with all tests passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,37 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numArr) {
+  let result = 0;
+  for(let i = 0; i < numArr.length; i++){
+    result = result + numArr[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(numArr) {
+  let result = 1;
+  for(let i = 0; i < numArr.length; i++){
+    result = result * numArr[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  if(num === 0){
+    return 1
+  } else{
+    return num * factorial(num-1);
+  }
 	
 };
 
