8153b7b4bd6b6d162b065baf887561935b0626ed
Crispon500 <crispon500@gmail.com>
2023-03-15 02:55:58 -0500

    exercise 9 complete with all tests passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let temp = str.toLowerCase().replace(/[^\w\s\']|_/g,"")
+                  .replace(/\s+/g, "");
+    temp.split("").join("");
+    return temp.split("").reverse().join("")== temp;
 };
 
 // Do not edit below this line
