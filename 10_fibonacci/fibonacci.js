0bd2530fdd6dfdda9aade1f03f58aad4d2f7b545
Crispon500 <crispon500@gmail.com>
2023-03-15 03:19:19 -0500

    exercise 10 complete with all tests passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let preNum = 1;
+    let postNum = 1;
+    let temp;
+    if(parseInt(num) <= 0){
+        return "OOPS";
+    } else if(parseInt(num) === NaN){
+        return "OOP"
+    } else if(parseInt(num) === 1){
+        return preNum;
+    } else{
+        for(let i = 1; i < num; i++){
+            temp = preNum + postNum;
+            preNum = postNum;
+            postNum = temp;
+        }
+        return preNum;
+    }
 
 };
 
