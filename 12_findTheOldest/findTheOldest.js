82f87bd73359a95e42968dd176dd70eaa5d277e1
Crispon500 <crispon500@gmail.com>
2023-03-15 21:23:13 -0500

    exercise 12 complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function (array) {
+    return array.reduce((oldest, current) => {
+      const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+      const currentAge = getAge(current.yearOfBirth,
+        current.yearOfDeath
+      );
+      return oldestAge < currentAge ? current : oldest;
+    });
+  };
+  
+  const getAge = function (birthYear, deathYear) {
+    if (!deathYear) {
+      deathYear = new Date().getFullYear();
+    }
+    return deathYear - birthYear;
+  };
 
 // Do not edit below this line
 module.exports = findTheOldest;
